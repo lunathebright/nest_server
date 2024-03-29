e0ee92cf1bd9105988e9e6bc68e9a83b38a94bc3
Luna <lunabright817@gmail.com>
2024-03-29 18:05:30 +0900

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// interface Post {
+//   author: string;
+//   title: string;
+//   content: string;
+//   likeCount: number;
+//   commentCount: number;
+// }
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
